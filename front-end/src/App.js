6c0e57daa8f5d3dbbf3e3c9fd26938ccc5f26bc2
hyunju1211 <hyunju0411@konkuk.ac.kr>
2024-07-21 01:07:54 +0900

    Updated category-button

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import './App.css';  
 
 const App = () => {
@@ -9,8 +10,19 @@ const App = () => {
         <div className="app-container">
             <Header />
             <Home />
-            {/* <Footer /> */}
+            {/* <Footer />  */}
         </div>
+        // 백엔드에서 링크 주면 아래로 path에다 url 업로드하기
+        // <Router>
+        //     <div className="app-container">
+        //         <Header />
+        //         <Routes>
+        //             <Route path="/" element={<Home />} />
+        //             <Route path="/" element={<Home />} />
+        //             <Route path="/" element={<Home />} />
+        //         </Routes>
+        //     </div>
+        // </Router>
     );
 };
 
--- front-end/src/pages/SupportPolicies.js
+++ front-end/src/pages/SupportPolicies.js
@@ -1,27 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SupportPolicies.css';
 
 const SupportPolicies = () => {
+
+      const [clickedButtons, setClickedButtons] = useState({
+        monitor: false,
+        cafeStyleOffice: false,
+        coworkingOffice: false,
+        Parking: false,
+        PhoneBoothl: false
+    });
+
+      const handleButtonClick = (buttonName) => {
+          setClickedButtons(prevState => ({
+              ...prevState,
+              [buttonName]: !prevState[buttonName],
+          }));
+      };
+
+
   return (
     <div className="support-policies">
       <div className="header-support-policies">
         <img className="text-image" alt="supportPolicies-img" src="/imgs/textImg.png" />
       </div>
       <div className="button-container">
-        <button className="category-button">바다🌊</button>
-        <button className="category-button">네트워킹파티🎉</button>
-        <button className="category-button">공유오피스</button>
-        <button className="category-button">더블모니터🖥️</button>
+                <button
+                    className={`category-button ${clickedButtons.monitor ? 'selected' : ''}`}
+                    onClick={() => handleButtonClick('monitor')}
+                >
+                    모니터
+                </button>
+                <button
+                    className={`category-button ${clickedButtons.cafeStyleOffice ? 'selected' : ''}`}
+                    onClick={() => handleButtonClick('cafeStyleOffice')}
+                >
+                    카페형 오피스
+                </button>
+                <button
+                    className={`category-button ${clickedButtons.coworkingOffice ? 'selected' : ''}`}
+                    onClick={() => handleButtonClick('coworkingOffice')}
+                >
+                    공유형 오피스
+                </button>
+                <button
+                    className={`category-button ${clickedButtons.Parking ? 'selected' : ''}`}
+                    onClick={() => handleButtonClick('Parking')}
+                >
+                    주차공간
+                </button>
+                <button
+                    className={`category-button ${clickedButtons.PhoneBooth ? 'selected' : ''}`}
+                    onClick={() => handleButtonClick('PhoneBooth')}
+                >
+                    PhoneBooth
+                </button>
       </div>
       <div className="cards">
+        <div className="card1">숙소이름(상품이름)</div>
         <div className="card">숙소이름(상품이름)</div>
         <div className="card">숙소이름(상품이름)</div>
         <div className="card">숙소이름(상품이름)</div>
         <div className="card">숙소이름(상품이름)</div>
         <div className="card">숙소이름(상품이름)</div>
         <div className="card">숙소이름(상품이름)</div>
-        <div className="card">숙소이름(상품이름)</div>
-
       </div>
     </div>
   );
