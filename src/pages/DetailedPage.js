113bcfe0e105bdf13aea52d9225fc5e6c6743b54
hyunju1211 <hyunju0411@konkuk.ac.kr>
2024-07-25 18:24:28 +0900

    Updated Overall design of DetailPage

--- src/pages/DetailedPage.js
+++ src/pages/DetailedPage.js
@@ -7,20 +7,20 @@ const DetailedPage = () => {
       <div className="gallery">
         <img className="large-image" src="/imgs/Landing-BG.png" alt="Gallery" />
         <img className="normal-image" src="/imgs/Landing-BG.png" alt="Gallery" />
-        <img className="normal-image" src="/imgs/Landing-BG.png" alt="Gallery" />
         <img className="TopRightRound-image" src="/imgs/Landing-BG.png" alt="Gallery" />
+        <img className="normal-image" src="/imgs/Landing-BG.png" alt="Gallery" />
         <img className="BottomRightRound-image" src="/imgs/Landing-BG.png" alt="Gallery" />
       </div>
       <main className="main">
         <div className="content">
           <section className="content-header">
             <h2>춘천 베어스호텔</h2>
-            <h5>한줄 소개글</h5>
+            <div className="lined-heading">한줄 소개글</div>
           </section>
           <section className="content-tagbar">
-            <div className="tag"></div>
-            <div className="tag"></div>
-            <div className="tag"></div>
+            <div className="tag">ㄹㄹㄹㄹㄹㄹ</div>
+            <div className="tag">ㅁㅁㅁ</div>
+            <div className="tag"ㅇㅇㅇ></div>
           </section>
           <section className="content-detail">
             <div className="share-office">
@@ -28,21 +28,21 @@ const DetailedPage = () => {
                 <h5>본문(숙소 설명 및)</h5>
             </div>
             <div className="share-office">
-                <h2>공유 오피스</h2>
+                <h2>숙소</h2>
                 <h5>본문(숙소 설명 및)</h5>
             </div>
             <div className="share-office">
-                <h2>공유 오피스</h2>
+                <h2>제공 혜택</h2>
                 <h5>본문(숙소 설명 및)</h5>
             </div>    
             <div className="share-office">
-                <h2>공유 오피스</h2>
+                <h2>기타 사항</h2>
                 <h5>본문(숙소 설명 및)</h5>
             </div>
           </section>
         </div>
         <div className="sidebar-container">
-          <div className="alarm">alarm</div>
+          <div className="alarm">지금 AI 써보러가기</div>
           <div className="sidebar">sidebar</div>
         </div>
       </main>
