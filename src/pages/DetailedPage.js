4ba3cd0f361cd8fc08bc6ff84bc8587a8523d958
hyunju1211 <hyunju0411@konkuk.ac.kr>
2024-08-05 21:22:44 +0900

    Updated DetailePageLayout

--- src/pages/DetailedPage.js
+++ src/pages/DetailedPage.js
@@ -20,36 +20,39 @@ const DetailedPage = () => {
   const [locationData, setLocationData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`http://ec2-15-164-115-210.ap-northeast-2.compute.amazonaws.com:8080/api/v1/location/info/${locationId}`);
-        setLocationData(response.data);
-        console.log('Fetched location data:', response.data); // Fetch된 데이터 로그 출력
-      } catch (err) {
-        setError('데이터를 가져오는 데 오류가 발생했습니다.');
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []); 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get(`http://ec2-15-164-115-210.ap-northeast-2.compute.amazonaws.com:8080/api/v1/location/info/${locationId}`);
+  //       setLocationData(response.data);
+  //       console.log('Fetched location data:', response.data); // Fetch된 데이터 로그 출력
+  //     } catch (err) {
+  //       setError('데이터를 가져오는 데 오류가 발생했습니다.');
+  //       console.error(err);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []); 
 
   // 태그 생성
-    const tags = [
-      // locationData.address,
-      // regionMap[locationData.region] || locationData.region,
-      locationData.monitor && '모니터',
-      locationData.conferenceRoom && '회의실',
-      locationData.parking && '주차공간',
-      locationData.phoneBooth && '폰부스',
-      locationData.officeType
-    ].filter(Boolean); // truthy 값만 필터링
-    console.log('Tags:', tags); // Tags 로그 출력
+    // const tags = [
+    //   // locationData.address,
+    //   // regionMap[locationData.region] || locationData.region,
+    //   locationData.monitor && '모니터',
+    //   locationData.conferenceRoom && '회의실',
+    //   locationData.parking && '주차공간',
+    //   locationData.phoneBooth && '폰부스',
+    //   locationData.officeType
+    // ].filter(Boolean); // truthy 값만 필터링
+    // console.log('Tags:', tags); // Tags 로그 출력
 
     
   const navigate = useNavigate(); //ai입력페이지로 이동
+    const handleNavigation = (url) => {
+      window.open(url, '_blank');
+    };
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -59,20 +62,22 @@ const DetailedPage = () => {
     navigate('/aipage');
   };
   return (
-    <div className="container">
-    <div className="gallery">
-        <img className="large-image" src={locationData.imageUrl} alt="Gallery" /> {/* 이미지 URL 사용 */}
+    <div className="Detailed-container">
+    <div className="Detailed-gallery">
+        {/* <img className="large-image" src={locationData.imageUrl} alt="Gallery" /> */}
+        <img className="large-image" src="imgs/Landing-BG.png" alt="Gallery" /> 
     </div>
     <main className="main">
-      <div className="content">
+      <div className="Detailed-content">
         <section className="content-header">
-          <h2>{locationData.name}</h2> {/* 이름 출력 */}
+          {/* <h2>{locationData.name}</h2> */}
+          <h2>가제</h2>
           <div className="lined-heading">한줄 소개글</div>
         </section>
         <section className="content-tagbar">
-          {tags.map((tag, index) => (
+          {/* {tags.map((tag, index) => (
             <div key={index} className="tag"># {tag}</div> 
-          ))}
+          ))} */}
         </section>
         <section className="content-detail">
           <div className="share-office">
@@ -93,7 +98,7 @@ const DetailedPage = () => {
           </div>
         </section>
       </div>
-        <div className="sidebar-container">
+        <div className="Detailed-sidebar-container">
           <div className="alarm-section">
             <button onClick={handleClick} className="alarm">Ai 도움받고 신청하기</button>
             <div className="alarm-message">
@@ -104,9 +109,30 @@ const DetailedPage = () => {
               지원동기 | 기대하는 바 | 활동계획
             </div>
           </div>
-          <div className="sidebar-box">
-            <div className="sidebar-message">📎 관련 사이트</div>
-            <div className="sidebar"></div>         
+          <div className="Detailed-sidebar-box">
+            <div className="Detailed-sidebar-message">📎 관련 사이트</div>
+            <div className="Detailed-sidebar">
+            <div className="Detailed" onClick={() => handleNavigation('https://worcation.sba.kr/S02/1/')}>
+                <h1> 1 서울기업워케이션 [퇴근만큼즐거운출근]</h1>
+                <img src="imgs/Seoul.png" alt="" className="Detailed-sideImg" />
+            </div>
+            <div className="Detailed" onClick={() => handleNavigation('https://jejuworkation.or.kr/Supportproject')}>
+                <h1> 2 제주 워케이션 </h1>
+                <img src="imgs/Jeju.png" alt="" className="Detailed-sideImg" />
+            </div>
+            <div className="Detailed" onClick={() => handleNavigation('https://worcation.co.kr/gw')}>
+                <h1> 3 강원 워케이션 </h1>
+                <img src="imgs/Gangwon.png" alt="" className="Detailed-sideImg" />
+            </div>
+            <div className="Detailed" onClick={() => handleNavigation('https://www.busaness.com/')}>
+                <h1> 4 부산 워케이션 </h1>
+                <img src="imgs/Busan.png" alt="" className="Detailed-sideImg" />
+            </div>
+            <div className="Detailed" onClick={() => handleNavigation('https://www.cacf.or.kr/site/program/index.php')}>
+                <h1> 5 충남 워케이션 (충남문화관광재단) </h1>
+                <img src="imgs/Chungnam.png" alt="" className="Detailed-sideImg2" />
+            </div>
+            </div>         
           </div>
         </div>
       </main>
